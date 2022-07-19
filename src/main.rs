4c7908a77896a85187e0a4f1e9ac4a60a720a353
Julian Osorio <juliancamilo.osorio@gmail.com>
2022-07-18 23:11:45 -0400

    Minimal request to OpenAI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{header::{AUTHORIZATION, CONTENT_TYPE}, Client};
+use std::env;
+
+static OPENAI_URL: &str = "https://api.openai.com/v1/completions";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_key = env::var("OPENAI_KEY")
+        .expect("Set the environment variable OPENAI_KEY to use for this bot");
+
+    let client = Client::new();
+
+    let request = client
+        .post(OPENAI_URL)
+        .header(CONTENT_TYPE, "application/json")
+        .header(AUTHORIZATION, format!("Bearer {}", api_key))
+        .body(r#"{"model": "text-davinci-002", "prompt": "Say this is a test", "temperature": 0, "max_tokens": 6}"#);
+    
+        println!("Request: {:?}", &request);
+        
+    let res = request.send().await?;
+
+    println!("Response: {:?}", &res.text().await?);
+
+    Ok(())
 }
